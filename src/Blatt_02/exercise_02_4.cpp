55e2c5be9523d94ba00188b7da02a6f31c0f4573
Christopher Pommrenke <christopher@pommrenke.com>
2011-11-03 14:09:24 +0100

    added solution for exercise 02_4

---
+++ src/Blatt_02/exercise_02_4.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+#include "hdnum.hh"
+#include "initial_value_problem.h"
+#include "expliciteuler.hh"							//from examplecode of hdnum
+
+// y - eps < x < y + eps ?
+bool inRange(const double x, const double y, const double eps) {
+	if(x > y - eps) {
+		if (x < y + eps) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+}
+
+template <class T>
+T computeConvergenceOrder(const T x1, const T x2, const T xExact) {
+  const T error1 = x1 - xExact;
+  const T error2 = x2 - xExact;
+  return log(fabs(error1 / error2)) / log(2.0);
+}
+
+template <class VECTOR>
+typename VECTOR::value_type computeAverage(const VECTOR& vec) {
+  typename VECTOR::value_type average = 0.0;
+  for(size_t i = 0; i < vec.size(); i++) {
+    average += vec.at(i);
+  }
+  average /= vec.size();
+  return average;
+}
+
+int main() {
+	typedef double Number;                    // define a number type
+
+	const Number t0 			      = -3.0;				// initial time
+	      Number tStep 			    = 0.00001;		// delta t
+	const Number tIntermediate 	= 1.0;        // state at this time will be printed on console
+	const Number uIntermediate  = 1.0 / (1.0 + (100.0 * tIntermediate * tIntermediate)); // exact state at tIntermediate
+	const Number tMax 			    = 3.0;				// end time
+	const Number u0 			      = 1.0/901.0;	// initial state
+	const Number lambda 		    = -200.0;			// factor for the model
+
+	const size_t iStart         = 5;
+	const size_t iEnd	          = 10;
+
+	hdnum::Vector<Number> statesIntermediate;     // store intermediate state values
+	for(size_t i = iStart; i <= iEnd; i++) {
+	  tStep = pow(2.0, -static_cast<double>(i));
+    typedef InitialValueProblem<Number> Model;  // Model type
+    Model model(lambda, u0, t0);					      // instantiate model
+
+    typedef ExplicitEuler<Model> Solver; 			  // Solver type
+    Solver solver(model);                			  // instantiate solver
+    solver.set_dt(tStep);                  			// set initial time step
+
+    hdnum::Vector<Number> times;           			// store time values here
+    hdnum::Vector<hdnum::Vector<Number> > states; 	// store states here
+    times.push_back(solver.get_time());  			// initial time
+    states.push_back(solver.get_state()); 			// initial state
+
+    while (solver.get_time() <= tMax) 				// the time loop
+    {
+      solver.step();                  				// advance model by one time step
+      times.push_back(solver.get_time()); 			// save time
+      states.push_back(solver.get_state()); 		// and state
+      if(inRange(tIntermediate, solver.get_time(), tStep/2.0)) {		// print state at intermediate time
+        //std::cout << "State at time t(delta t = " << tStep << ") = " << solver.get_time() << ": " << solver.get_state() << std::endl;
+        statesIntermediate.push_back(solver.get_state().at(0));
+      }
+    }
+	}
+
+	hdnum::Vector<Number> convergenceOrders;     // store computed convergence orders
+	for(size_t i = 0; i < iEnd - iStart; i++) {
+	  convergenceOrders.push_back(computeConvergenceOrder(statesIntermediate.at(i), statesIntermediate.at(i + 1), uIntermediate));
+	  std::cout << "convergence order for h = " << pow(2.0, -static_cast<double>(i + iStart)) << " : " << convergenceOrders.back() << std::endl;
+	}
+
+	Number averageConvergenceOrder = computeAverage(convergenceOrders);
+	std::cout << "average convergence order: " << averageConvergenceOrder << std::endl;
+	return 0;
+}
---
+++ src/Blatt_02/initial_value_problem.h
@@ -0,0 +1,63 @@
+#ifndef INITIAL_VALUE_PROBLEM_H_
+#define INITIAL_VALUE_PROBLEM_H_
+
+/** @brief Example class for a differential equation model
+
+    The model is
+
+    u'(t) = lambda*t*u²(t), -3 <= t <= 3, u(t_0) = 1 / 901.
+
+    \tparam T a type representing time values
+    \tparam N a type representing states and f-values
+*/
+template<class T, class N=T>
+class InitialValueProblem {
+private:
+  const N lambda;
+  const N u0;
+  const N t0;
+
+public:
+  /** \brief export size_type */
+  typedef std::size_t size_type;
+
+  /** \brief export time_type */
+  typedef T time_type;
+
+  /** \brief export number_type */
+  typedef N number_type;
+
+  //! constructor stores parameter lambda
+  InitialValueProblem (const N& lambda_, const N& u0_, const N& t0_)
+    : lambda(lambda_), u0(u0_), t0(t0_)
+  {}
+
+  //! return number of componentes for the model
+  std::size_t size () const
+  {
+    return 1;
+  }
+
+  //! set initial state including time value
+  void initialize (T& t0, hdnum::Vector<N>& x0) const
+  {
+    t0 = this->t0;
+    x0[0] = u0;
+  }
+
+  //! model evaluation
+  void f (const T& t, const hdnum::Vector<N>& x, hdnum::Vector<N>& result) const
+  {
+    result[0] = lambda*t*x[0]*x[0];
+  }
+
+  //! jacobian evaluation needed for implicit solvers
+  void f_x (const T& t, const hdnum::Vector<N>& x, hdnum::DenseMatrix<N>& result) const
+  {
+    throw std::string("Jacobian evaluation not implemented!");
+  }
+};
+
+
+
+#endif /* INITIAL_VALUE_PROBLEM_H_ */
