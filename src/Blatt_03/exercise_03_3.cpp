087f5339c33d1d91b4d20bb32fb522c28e079f96
Christopher Pommrenke <christopher@pommrenke.com>
2011-11-11 07:53:13 +0100

    updated exercise 03_3

--- src/Blatt_03/exercise_03_3.cpp
+++ src/Blatt_03/exercise_03_3.cpp
@@ -50,44 +50,63 @@ int main() {
   const size_t iEnd           = 8;
 
   hdnum::Vector<Number> statesIntermediate;     // store intermediate state values
+  hdnum::Vector<Number> statesIntermediate2;     // store intermediate state values
   for(size_t i = iStart; i <= iEnd; i++) {
     tStep = pow(2.0, -static_cast<double>(i));
     typedef InitialValueProblem<Number> Model;  // Model type
     Model model(lambda, u0, t0);                // instantiate model
 
     typedef ExplicitEuler<Model> Solver;        // Solver type
-    //typedef HeunsMethod<Model> Solver;        // Solver type
+    typedef HeunsMethod<Model> Solver2;        // Solver type
     Solver solver(model);                       // instantiate solver
     solver.set_dt(tStep);                       // set initial time step
+
+    Solver2 heuns_method(model);
+    heuns_method.set_dt(tStep);
 
     hdnum::Vector<Number> times;                // store time values here
     hdnum::Vector<hdnum::Vector<Number> > states;   // store states here
     times.push_back(solver.get_time());       // initial time
     states.push_back(solver.get_state());       // initial state
 
+    hdnum::Vector<Number> times2;                // store time values here
+    hdnum::Vector<hdnum::Vector<Number> > states2;   // store states here
+    times2.push_back(heuns_method.get_time());       // initial time
+    states2.push_back(heuns_method.get_state());       // initial state
+
     while (solver.get_time() < tMax)         // the time loop
     {
       solver.step();                          // advance model by one time step
+      heuns_method.step();
       times.push_back(solver.get_time());       // save time
       states.push_back(solver.get_state());     // and state
+      times2.push_back(heuns_method.get_time());       // save time
+      states2.push_back(heuns_method.get_state());     // and state
       if(inRange(tIntermediate, solver.get_time(), tStep/2.0)) {    // print state at intermediate time
         //std::cout << "State at time t(delta t = " << tStep << ") = " << solver.get_time() << ": " << solver.get_state() << std::endl;
         statesIntermediate.push_back(solver.get_state().at(0));
+        statesIntermediate2.push_back(heuns_method.get_state().at(0));
       }
     }
     if(i == iEnd) {
-      gnuplot("exercise_03_3.dat",times,states);    // output model result
+      gnuplot("exercise_03_3_euler.dat",times,states);    // output model result
+      gnuplot("exercise_03_3_heun.dat",times2,states2);    // output model result
     }
   }
 
   hdnum::Vector<Number> convergenceOrders;     // store computed convergence orders
+  hdnum::Vector<Number> convergenceOrders2;     // store computed convergence orders
   for(size_t i = 0; i < iEnd - iStart; i++) {
     convergenceOrders.push_back(computeConvergenceOrder(statesIntermediate.at(i), statesIntermediate.at(i + 1), uIntermediate));
-    std::cout << "convergence order for h = " << pow(2.0, -static_cast<double>(i + iStart)) << " : " << convergenceOrders.back() << std::endl;
+    std::cout << "euler convergence order for h = " << pow(2.0, -static_cast<double>(i + iStart)) << " : " << convergenceOrders.back() << std::endl;
+    convergenceOrders2.push_back(computeConvergenceOrder(statesIntermediate2.at(i), statesIntermediate2.at(i + 1), uIntermediate));
+    std::cout << "heun convergence order for h = " << pow(2.0, -static_cast<double>(i + iStart)) << " : " << convergenceOrders2.back() << std::endl;
   }
 
   Number averageConvergenceOrder = computeAverage(convergenceOrders);
-  std::cout << "average convergence order: " << averageConvergenceOrder << std::endl;
+  std::cout << "euler average convergence order: " << averageConvergenceOrder << std::endl;
+  Number averageConvergenceOrder2 = computeAverage(convergenceOrders2);
+  std::cout << "heun average convergence order: " << averageConvergenceOrder2 << std::endl;
 
   return 0;
 }
--- src/Blatt_03/plotscript.p
+++ src/Blatt_03/plotscript.p
@@ -7,9 +7,12 @@ set ytic auto                          # set ytics automatically
 #set yr [-0.1:1.1]
 
 set terminal postscript enhanced color
-set title "Approximation of u(t) for u'(t) = -t / u(t)"
+set title "Euler Approximation of u(t) for u'(t) = -t / u(t)"
 set xlabel "t"
 set ylabel "u(t)"
-set output "exercise_03_3.ps"
-plot "exercise_03_3.dat" with lines
+set output "exercise_03_3_euler.ps"
+plot "exercise_03_3_euler.dat" with lines
 
+set title "Heun Approximation of u(t) for u'(t) = -t / u(t)"
+set output "exercise_03_3_heun.ps"
+plot "exercise_03_3_heun.dat" with lines
