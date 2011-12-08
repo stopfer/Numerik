ad574680f97ec491b630b5d47fa5cdd3ff48031d
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2011-12-08 20:14:17 +0100

    progr. aufgabe sollte fertig sein

--- src/Blatt_07/exercise_7.cpp
+++ src/Blatt_07/exercise_7.cpp
@@ -29,11 +29,9 @@ public:
 
         hdnum::Vector<number_type> iterValue = u;
         number_type tol = 0.001;
-        number_type error = 1000000;
-
+        number_type error = 100000;
+        //newton solve
         while (error > tol) {
-            //evaluate the "help" functions fot the
-            //newton solver
             hdnum::DenseMatrix<number_type> A;
             hdnum::Vector<number_type> b;
             this->evaluateNewtonJacobiFunction(iterValue, A);
@@ -54,7 +52,7 @@ public:
             //update the value         
             newValue = iterValue + z;
 
-            //compute erroror
+            //compute error
             tmp = newValue - iterValue;
             error = tmp.two_norm() / iterValue.two_norm();
             iterValue = newValue;
@@ -68,7 +66,9 @@ public:
         const hdnum::Vector<number_type>& inputVector,
         hdnum::Vector<number_type>& resultVectors
     ) {
-        //  F = @(x) x - (h/2)*f(t+h, x) - y - (h/2)*f(t,y); % Hilfsfunktion für Newton
+        // "sub" function which is passed to the
+        // newton solver
+        
         hdnum::Vector<number_type> t1;
         hdnum::Vector<number_type> t2;
         model.f(t + dt, inputVector, t1);
@@ -84,44 +84,39 @@ public:
     (
             const hdnum::Vector<number_type>& inputVector,
             hdnum::DenseMatrix<number_type>& resultMatrix
-        ) {
+    ) {
+        // jacobi matrix for the "sub" model
+        // which is passed to the newton solver
+        
         hdnum::DenseMatrix<number_type> temp;
         hdnum::DenseMatrix<number_type> identity(inputVector.size(), inputVector.size(), 0);
+        // identity
         for (size_t i = 0; i < inputVector.size(); ++i)
             identity(i, i) = 1;
+        // evaluate the jacobi matrix of the model
         model.f_x(t + dt, u, temp);
         for(size_t c=0;c<temp.colsize();++c)
         for(size_t r=0;r<temp.rowsize();++r)
             temp(r,c)*=(dt / 2.0) ;
         resultMatrix = identity - temp;
     }
-
     //! set current state
-
     void set_state(time_type t_, const hdnum::Vector<number_type>& u_) {
         t = t_;
         u = u_;
     }
-
     //! get current state
-
     const hdnum::Vector<number_type>& get_state() const {
         return u;
     }
-
     //! get current time
-
     time_type get_time() const {
         return t;
     }
-
     //! get dt used in last step (i.e. to compute current state)
-
     time_type get_dt() const {
         return dt;
     }
-
-
 private:
     const Model & model;
     time_type t, dt;
@@ -134,37 +129,28 @@ private:
 
 int main() {
     typedef double Number; // define a number type
-
     const Number t0 = 0.0; // initial time
-    Number tStep = 0.01; // delta t
-    const Number tMax = 10000; // end time
-
+    Number tStep = 0.005; // delta t
+    const Number tMax = 100000; // end time
     hdnum::Vector<Number> u0(2); // initial state
-
-
     //set values of u0 and A
     u0[0] = 3;
     u0[1] = 4;
-
-
     typedef InitialValueProblem<Number> Model; // Model type
     Model model(u0, t0); // instantiate model
     typedef ImplicitTrapezSolver<Model> Solver; // solver
     Solver solver(model); // instantiate solver
-
     // initialize solver
     solver.set_dt(tStep);
     solver.set_state(t0, u0);
-
+    size_t iter=0;
     while (solver.get_time() <= tMax) // the time loop
     {
         solver.step(); // advance model by one time step
-        std::cout<<"u0 for t="<<tMax<<" ="<<solver.get_state()[0]<<"\n"
-            <<"u1 for t="<<tMax<<" ="<<solver.get_state()[1]<<"\n";
+        // cout for convergence information
+        if(iter%100000==0)
+            std::cout<<"u0 for t="<<solver.get_time()<<" ="<<solver.get_state()[0]<<" u1  ="<<solver.get_state()[1]<<"\n";
+        ++iter;
     }
-    
-    std::cout<<"u0 for t="<<tMax<<" ="<<solver.get_state()[0]<<"\n"
-            <<"u1 for t="<<tMax<<" ="<<solver.get_state()[1]<<"\n";
-
     return 0;
-}
+ }
